474fc2237b7fa4046562b63cb8935b5b73773be0
atnuhs-1 <syunnta0033@gmail.com>
2024-05-03 21:15:50 +0900

    add image, modify testPage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { TodoData } from "./types/types";
 import TodoList from "./components/todolist";
 import FormArea from "./components/formArea";
 import { getAllTodo } from "./actions/postTodoAction";
+import Image from "next/image";
 
 export default async function Home() {
   async function getTodoAllData() {
@@ -16,11 +17,14 @@ export default async function Home() {
   const todoAllData = await getAllTodo();
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-96">
-        <FormArea />
-        <TodoList todoAllData={todoAllData} />
+    <>
+      <Image src="/amamori1.jpeg" width={2048} height={500} alt="" className="flex justify-center items-center"/>
+      <div className="flex h-screen justify-center">
+        <div className="w-96">
+          <FormArea />
+          <TodoList todoAllData={todoAllData} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -6,28 +6,41 @@ const todos = [
   { id: 0, text: "a" },
 ];
 
+const items = ["item1", "item2", "item3", "item4", "item5", "item6"];
+
 const TestPage = () => {
   return (
-    <main className="flex h-screen items-center justify-center">
-      <div className="w-96">
-        <h1 className="sr-only">Todos</h1>
-        <div className="text-right"></div>
-        {/* <AddForm /> */}
-        <button className="my-2 w-full border-red-500">add</button>
-        <ul>
-          {todos.map((todo) => (
-            <li
-              key={todo.id}
-              className="mb-2.5 flex items-center justify-between rounded border bg-slate-100 p-2.5 dark:bg-slate-900"
-            >
-              <span className="mr-4">{todo.text}</span>
-              {/* <DeleteForm id={todo.id} todo={todo.text} /> */}
-              <button>delete</button>
-            </li>
+    <>
+      <main className="flex h-screen items-center justify-center">
+        <div className="w-96">
+          <h1 className="sr-only">Todos</h1>
+          <div className="text-right"></div>
+          {/* <AddForm /> */}
+          <button className="my-2 w-full border-red-500">add</button>
+          <ul>
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className="mb-2.5 flex items-center justify-between rounded border bg-slate-100 p-2.5 dark:bg-slate-900"
+              >
+                <span className="mr-4">{todo.text}</span>
+                {/* <DeleteForm id={todo.id} todo={todo.text} /> */}
+                <button>delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+      <div>
+        <h1>grid grid-cols-3</h1>
+        <div className="p-4 grid grid-cols-4">
+          {items.map((item) => (
+            <div key={item}>{item}</div>
           ))}
-        </ul>
+          <div className="grid-cols-subgrid">item8</div>
+        </div>
       </div>
-    </main>
+    </>
   );
 };
 
